e93d13f6f792442caf6833782512b55caa61e45a
Tanmayi D <60173146+tanmayivenkatesh@users.noreply.github.com>
2021-08-12 20:34:25 +0530

    Made changes in settings.py file

--- LaunchED/settings.py
+++ LaunchED/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LaunchED.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'static')],
+        'DIRS': [os.path.join(BASE_DIR,'static'), os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
