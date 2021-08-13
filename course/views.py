3d0cc9594c1d9d86731afcbb5f2146bcd27a7d05
Tanmayi D <60173146+tanmayivenkatesh@users.noreply.github.com>
2021-08-13 14:12:45 +0530

    Made some minor changes

--- course/views.py
+++ course/views.py
@@ -1,4 +1,5 @@
 from django.http.response import HttpResponse
+from django.http import request
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import get_user_model
@@ -13,10 +14,20 @@ def home(request):
     return render(request,"home.html")
 
 def courses(request):
-    return HttpResponse("Courses Page!")
+    course = Course.objects.all() 
+    COURSE = {
+        "courses":course 
+    }
+    return render(request, 'courses.html',COURSE)
 
 def instructors(request):
-    return HttpResponse("Instructors Page!")
+    instructor = Instructor.objects.all()
+    course = Course.objects.all()
+    INSTRUCTOR = {
+        "instructors":instructor,
+        "courses":course
+    }
+    return render(request, 'instructors.html',INSTRUCTOR)
 
 def login(request):
     if request.method == "POST":
@@ -35,7 +46,9 @@ def login(request):
         return render(request, 'login.html')
 
 def logout(request):
-    return HttpResponse("Logout Page")
+    auth.logout(request)
+    messages.success(request,"You have successfully logged off!")
+    return redirect("/")
 
 def registerStudent(request):
     if request.method == "POST":
@@ -62,7 +75,7 @@ def registerStudent(request):
 
 def registerInstructor(request):
     if request.method == "POST":
-        f_name = request.POST['fname']
+        f_name = request.POST.get('fname')
         l_name = request.POST.get('lname')
         username = request.POST.get('email')
         password1 = request.POST.get('password1')
@@ -83,29 +96,127 @@ def registerInstructor(request):
     else:
         return render(request, 'registerInstructor.html')
 
+
 def profile(request):
-    return HttpResponse("Profile")
+    if request.user.is_student():
+        student = Student.objects.get(email = request.user.username)
+        courses = Course.objects.all()
+        enrolled_courses = StudentCourse.objects.filter(student__email__startswith=request.user.username)
+        context = {
+            "Students": student,
+            "Courses": courses,
+            "enrolled": enrolled_courses,
+        }
+        return render(request,"profile.html",context)
+    if request.user.is_instructor():
+        instructor = Instructor.objects.get(email = request.user.username)
+        courses = instructor.course_set.all()
+        context = {
+            "Instructors":instructor,
+            "courses" : courses
+        }
+        return render(request,"profile.html", context)
 
 def deleteStudentCourse(request, course_id):
-    return HttpResponse("Deleted Student Course:"+str(course_id))
+    course = StudentCourse.objects.get(pk=course_id)
+    course.delete()
+    return redirect('/profile')
 
 def profileDelete(request):
-    return HttpResponse("Profile Delete")
+    if request.user.is_student():
+        student=Student.objects.get(email = request.user.username)
+        UsEr = User.objects.get(username = request.user.username)
+        student.delete()
+        UsEr.delete()
+    if request.user.is_instructor():
+        instructor=Instructor.objects.get(email = request.user.username)
+        UsEr = User.objects.get(username = request.user.username)
+        instructor.delete()
+        UsEr.delete()
+    messages.success(request,"Account successfully deleted, We're really sorry to see you leave us :'(  ")
+    return redirect('/')
 
 def profileUpdate(request):
-    return HttpResponse("Profile Update")
+    if request.method == "POST":
+        if request.user.is_student():
+            curr_user=Student.objects.filter(email = request.user.username)
+        if request.user.is_instructor():
+            curr_user=Instructor.objects.filter(email = request.user.username)
+        f_name = request.POST.get('fname')
+        l_name = request.POST.get('lname')
+        DOB = request.POST.get('DOB')
+        qual = request.POST.get('qualification')
+        if request.user.is_instructor():
+            About = request.POST.get('About')
+        if (len(DOB) == 0):
+            messages.success(request,"DOB field cannot be empty!")
+            return redirect('profileUpdate')
+        else:
+            form = curr_user.update(fname=f_name,lname=l_name,qualification=qual,DOB=DOB)
+            if request.user.is_instructor():
+                curr_user.update(description = About)
+            form_user = USER.objects.filter(username = request.user.username)
+            form_user.update(first_name=f_name, last_name=l_name)
+            messages.success(request,"Successfully changed profile details")
+            return redirect('/')
+    else:
+        return render(request,'profileUpdate.html')
 
 def createCourse(request):
-    return HttpResponse("Create Course")
+    certifier = Certifier.objects.all()
+    instructor = Instructor.objects.get(email = request.user.username)
+    courses = instructor.course_set.all()
+    context = {
+            "certifier" : certifier,
+            "Instructors":instructor,
+            "courses" : courses
+        }
+    if request.method == "POST":
+        course_name = request.POST.get('Cname')
+        certifier = request.POST.get('Ccertifier')
+        course_description = request.POST.get('Cdesc')
+        if(len(course_name) == 0 and len(course_description) == 0):
+            messages.success(request,"Invalid form, Please fill everyblock!")
+            return redirect('createCourse')
+        else:
+            certifier_name = Certifier.objects.get(id=certifier)
+            inst_name = Instructor.objects.get(email=request.user.username)
+            form = Course.objects.create(name=course_name,description=course_description,certifier=certifier_name,instructor=inst_name)
+            form.save()
+        return redirect('/profile')
+
+    return render(request,"createCourse.html",context)
 
 def deleteInstructorCourse(request, course_id):
-    return HttpResponse("Deleted Instructor Course:"+str(course_id))
+    course = Course.objects.get(pk=course_id)
+    course.delete()
+    return redirect('/profile')
 
 def myCourse(request):
-    return HttpResponse("My Course")
+    student = Student.objects.get(email = request.user.username)
+    courses = Course.objects.all()
+    enrolled_courses = StudentCourse.objects.filter(student__email__startswith=request.user.username)
+    context = {
+            "Students": student,
+            "Courses": courses,
+            "enrolled": enrolled_courses,
+        }
+    if request.method == "POST":
+        course_name = request.POST.get('Cname')
+        c_id = Course.objects.get(id=course_name)
+        s_name= Student.objects.get(email=request.user.username)
+    return render(request,"myCourses.html",context)
 
 def enroll(request, course_id):
-    return HttpResponse("Enroll Course is "+str(course_id))
+    course_id = Course.objects.get(id=course_id)
+    student_id = Student.objects.get(email = request.user.username)
+    if student_id.studentcourse_set.filter(course__id__contains=course_id.id).exists() == True:
+        messages.success(request,"You are already enrolled in this course! select another one!")
+        return redirect('/courses')
+    else:
+        form = StudentCourse.objects.create(course=course_id,student=student_id)
+        form.save()
+        return redirect('/profile')
 
 def sampleCertificate(request):
     return HttpResponse("Sample Certificate")
